02251e5e88b2795cda0266ee8dd0baa2b4d36a1c
Derek <legato_5@yahoo.com>
2019-09-30 20:42:41 -0700

    added links on navigation to correct routes

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ const App = () => (
   <div>
     <Navigation />
 
-    <Route exact path="/" />
-    <Route path="/about" />
-    <Route path="/contact" />
+    <Route exact path="/" component={Home}/>
+    <Route path="/about" component={About}/>
+    <Route path="/contact" component={Contact}/>
   </div>
 );
 
